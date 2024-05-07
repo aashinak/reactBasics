5c45f3411e4d04e061b1da048a343c2831193446
Aashin <aashin9605@gmail.com>
2024-05-07 13:55:41 +0530

    complete reduxTodo

--- reduxTodo/src/components/AddToDo.jsx
+++ reduxTodo/src/components/AddToDo.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react"
+import { useDispatch } from 'react-redux'
+import { addTodo } from "../features/todo/todoSlice"
 
 function AddToDo() {
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+  const addTodoHandler = (e) => {
+    e.preventDefault()
+    dispatch(addTodo(input))
+    setInput('')
+  }
   return (
-    <div className="w-full bg-slate-600 px-7 py-5 flex justify-between rounded-lg shadow-lg">
-        <input type="text" className="p-2 bg-transparent border w-3/4 border-white rounded-lg" placeholder="Enter toDo" name="" id="" />
-        <button className="bg-blue-800 p-2 text-white rounded-lg">Add Todo</button>
+    <div className="w-full bg-slate-600 px-7 py-5  rounded-lg shadow-lg">
+      <form onSubmit={addTodoHandler} className="flex justify-between" >
+
+        <input type="text" className="p-2 focus:outline-none text-white font-semibold bg-transparent border w-3/4 border-white rounded-lg" 
+          placeholder="Enter toDo" 
+          name="" id=""
+          value={input}
+          onChange={(e)=> setInput(e.target.value)} />
+
+          <button className="bg-blue-800 p-2 text-white rounded-lg"
+          type="submit"
+          >Add Todo</button>
+
+      </form>
+        
     </div>
   )
 }
--- reduxTodo/src/components/Todo.jsx
+++ reduxTodo/src/components/Todo.jsx
@@ -1,11 +1,23 @@
+import {useSelector ,useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todo() {
+
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
   return (
     <>
-        <div className="mt-5 w-full bg-slate-400 p-4 rounded-lg flex justify-between items-center shadow-lg">
-            <label className="text-white font-semibold" htmlFor="">Do homwn</label>
-            <button className="btn bg-gray-200 p-1 rounded-lg w-24 text-red-700"><i className="fa fa-trash mr-1"></i>DELETE</button>
+      {todos.map((todo) => (
+        <div key={todo.id} className="mt-5 w-full bg-slate-400 p-4 rounded-lg flex justify-between items-center shadow-lg">
+        <label className="text-white font-semibold" htmlFor="">{todo.text}</label>
+        <button 
+        onClick={() => dispatch(removeTodo(todo.id))}
+        className="btn bg-gray-200 p-1 rounded-lg w-24 text-red-700"><i className="fa fa-trash mr-1"></i>DELETE</button>
         </div>
+      ))}
+
+
+        
         
     </>
     
